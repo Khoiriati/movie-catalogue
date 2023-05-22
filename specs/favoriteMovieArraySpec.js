a56ef68c855b42d8018e66f4360e0e86a26de76e
Khoiriati <sitikhoiriati@gmail.com>
2023-05-22 10:13:44 +0700

    Initialize movie catalogue automated testing implemented

---
+++ specs/favoriteMovieArraySpec.js
@@ -0,0 +1,52 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable consistent-return */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-const-assign */
+/* eslint-disable no-return-assign */
+import { itActAsFavoriteMovieModel } from './contract/favoriteMovieContract';
+
+let favoriteMovies = [];
+
+const FavoriteMovieArray = {
+  getMovie(id) {
+    if (!id) {
+      return;
+    }
+    return favoriteMovies.find((movie) => movie.id === id);
+  },
+
+  getAllMovies() {
+    return favoriteMovies;
+  },
+
+  putMovie(movie) {
+    if (!movie.hasOwnProperty('id')) {
+      return;
+    }
+    if (this.getMovie(movie.id)) {
+      return;
+    }
+    favoriteMovies.push(movie);
+  },
+
+  deleteMovie(id) {
+    favoriteMovies = favoriteMovies.filter((movie) => movie.id !== id);
+  },
+
+  searchMovies(query) {
+    return this.getAllMovies()
+      .filter((movie) => {
+        const loweredCaseMovieTitle = (movie.title || '-').toLowerCase();
+        const jammedMovieTitle = loweredCaseMovieTitle.replace(/\s/g, '');
+        const loweredCaseQuery = query.toLowerCase();
+        const jammedQuery = loweredCaseQuery.replace(/\s/g, '');
+        return jammedMovieTitle.indexOf(jammedQuery) !== -1;
+      });
+  },
+};
+
+describe('Favorite Movie Array Contract Test Implementation', () => {
+  afterEach(() => favoriteMovies = []);
+
+  itActAsFavoriteMovieModel(FavoriteMovieArray);
+});
